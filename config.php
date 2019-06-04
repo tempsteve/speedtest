f8fd53227f02c38efad582970300cc2d24c52710
Steve <steve@boxful.com.tw>
2019-06-04 11:52:58 +0800

    4

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $servers = '
 var SPEEDTEST_SERVERS=[
     {
 		name:"Speedtest Server 1 (192.168.135.38)",
-		server:"http://192.168.135.38/",
+		server:"http://192.168.135.38:60080/speedtest",
 		dlURL:"garbage.php",
 		ulURL:"empty.php",
 		pingURL:"empty.php",
@@ -14,22 +14,6 @@ var SPEEDTEST_SERVERS=[
     {
 		name:"Speedtest Server 2 (192.168.135.39)",
 		server:"http://192.168.135.39:60080/speedtest/",
-		dlURL:"garbage.php",
-		ulURL:"empty.php",
-		pingURL:"empty.php",
-		getIpURL:"getIP.php"
-	},
-    {
-		name:"Hinet",
-		server:"http://168.95.1.1/",
-		dlURL:"garbage.php",
-		ulURL:"empty.php",
-		pingURL:"empty.php",
-		getIpURL:"getIP.php"
-	},
-    {
-		name:"Google",
-		server:"http://8.8.8.8/",
 		dlURL:"garbage.php",
 		ulURL:"empty.php",
 		pingURL:"empty.php",
